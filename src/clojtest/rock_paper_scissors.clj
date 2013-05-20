32bc0107cf376dfd81feb90b886cab6bab9770e9
Joseph Miller <joff@redbubble.com>
2013-05-20 13:37:57 +1000

    Player choices updating, function to display scores

--- src/clojtest/rock_paper_scissors.clj
+++ src/clojtest/rock_paper_scissors.clj
@@ -30,17 +30,17 @@
   (update-player [self _ _] self)
   )
 
-(defrecord Stubborn [choice]
+(defrecord StubbornPlayer [choice]
   Player
   (choose [_] choice)
   (update-player [this _ _] this))
 
-(defrecord Mean [last-winner]
+(defrecord MeanPlayer [last-winner]
   Player
   (choose [_]
     (if last-winner last-winner (rand-nth choices)))
   (update-player [_ me you]
-    (->Mean (when (iwon? me you) me))))
+    (->MeanPlayer (when (iwon? me you) me))))
 
 
 ; {player1 score, player2 score}
@@ -50,12 +50,14 @@
   (let [ p1 (first (keys scores))
          p2 (second (keys scores))
          match-choices [(choose p1) (choose p2)] ]
-    (prn match-choices)
+;    (prn match-choices)
+    (update-player p1 (first match-choices) (second match-choices))
+    (update-player p2 (second match-choices) (first match-choices))
     (if (apply draw? match-choices)
       scores
       (if (apply iwon? match-choices)
-        (update-in scores [p1] inc)   ; increment score for me, update player
-        (update-in scores [p2] inc)))))      ; increment score for them, update player
+          (update-in scores [p1] inc)   ; increment score for me, update player
+          (update-in scores [p2] inc)))))      ; increment score for them, update player
 
 (defn game [player1 player2 rounds]
   (reduce
@@ -63,3 +65,10 @@
     {player1 0, player2 0} ; initial map
       (range 0 rounds) ; sequence to loop
     ))
+
+(defn show-result [game-result]
+  (let [p1-score (first (vals game-result))
+         p2-score (second (vals game-result))]
+     (println "Player 1: " p1-score)
+     (println "Player 2: " p2-score)))
+
